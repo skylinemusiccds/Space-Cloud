0bba39ee511efb6b990c3cad38e996ef16ed0cf5
Satyam Singh <129391643+skylinemusiccds@users.noreply.github.com>
2024-03-30 11:14:13 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,8 +4,8 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "drive-b3808.firebaseapp.com",
-  projectId: "drive-b3808",
+  authDomain: "spacecloud-efc6d.firebaseapp.com",
+  projectId: "spacecloud-efc6d",
   storageBucket: "drive-b3808.appspot.com",
   messagingSenderId: "81678356040",
   appId: "1:81678356040:web:17ad6998200104ea22127d",
